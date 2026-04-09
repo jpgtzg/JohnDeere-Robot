6a14c0f652602d111ce1ebe59fbcd59abdd1006b
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2026-04-08 21:24:14 -0600

    Update USART configuration for 115200 baud rate and add receive function

--- Src/user_uart.c
+++ Src/user_uart.c
@@ -12,10 +12,11 @@ void USER_USART1_Init( void ){
 	USART1->CR2		&=	~USART_CR2_STOP;//		Step 3 1 Stop bit
 	USART1->BRR	 	|=	 USARTDIV;//			Step 5 Desired baud rate
 	USART1->CR1		|= 	 USART_CR1_TE;//		Step 6 Transmitter enabled
+	USART1->CR1		|= 	 USART_CR1_RE;//		Step 6 Receiver enabled
 
-	// Configuring RX pin (PA10) as Alternate function push pull -> 10 01
-	GPIOA->CRH		&=	~( 0x1UL << 10U ) & ~( 0x2UL << 8U );
-	GPIOA->CRH		|=	 ( 0x2UL << 10U ) |	 ( 0x1UL << 8U );
+	// Configuring RX pin (PA10) as input floating -> CNF=01, MODE=00
+	GPIOA->CRH		&=	~( 0xFUL << 8U );//		clear MODE10[1:0] and CNF10[1:0]
+	GPIOA->CRH		|=	 ( 0x1UL << 10U );//	CNF10=01 (floating input), MODE10=00
 }
 
 void USER_USART1_Transmit( uint8_t *pData, uint16_t size ){
@@ -27,6 +28,11 @@ void USER_USART1_Transmit( uint8_t *pData, uint16_t size ){
 static void USER_USART1_Send_8bit( uint8_t Data ){
 	while(!( USART1->SR & USART_SR_TXE ));//	wait until next data can be written
 	USART1->DR = Data;//				Step 7 Data to send
+}
+
+uint8_t USER_USART1_Receive_8bit( void ){
+	while(!( USART1->SR & USART_SR_RXNE ));//	wait until data is received
+	return (uint8_t)USART1->DR;//			read and return received data
 }
 
 int _write(int file, char *ptr, int len){
